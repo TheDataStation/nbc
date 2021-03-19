97a5e2f9620f4005a0cdb34562d9620e6d0d2442
Ben Clifford <benc@hawaga.org.uk>
2021-03-19 13:58:14 +0000

    add setup.py to make this installable with pip, or pip -e at least

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    install_requires = f.readlines()
+
+setuptools.setup(
+    name="aurum-datadiscovery", # Replace with your own username
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="aurum dod (short) description",
+    install_requires=install_requires,
+    long_description="aurum dod long description",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.6,<3.7",
+)
